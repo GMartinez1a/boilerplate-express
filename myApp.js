69e318b4157e0db446bd85b69a72c460db9595ff
George Martinez <georgemartinez0014@gmail.com>
2020-06-02 00:14:19 -0700

    8) Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -43,7 +43,12 @@ function logger(req, res, next){
 };
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
 
