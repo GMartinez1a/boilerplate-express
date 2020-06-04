2e1dc0bb7bf6c70276b8961b765dabcd9b2573c8
George Martinez <georgemartinez0014@gmail.com>
2020-06-04 12:07:23 -0700

    12) Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -58,17 +58,23 @@ app.get('/:word/echo', (req, res)=>{
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-app.get('/name', (req, res)=>{
+/*app.get('/name', (req, res)=>{
     let {first, last} = req.query;
     res.json({name: `${first} ${last}`})
-});
+});*/
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 
 /** 12) Get data form POST  */
-
+app.route('/name').get((req, res)=>{
+    let {first, last} = req.query;
+    res.json({name: `${first} ${last}`})
+}).post((req, res)=>{
+    let {first, last} = req.body;
+    res.json({name: `${first} ${last}`})
+});
 
 
 // This would be part of the basic setup of an Express app
